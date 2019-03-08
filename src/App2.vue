5fd05c02052e1b7b65674fc3d788f8b1ca060e1d
Virginia Contreras <vcontreras@americatv.pe>
2019-03-07 20:44:50 -0500

    Cambios se ubican por dia en x

--- src/App2.vue
+++ src/App2.vue
@@ -1,15 +1,15 @@
 <template>
   <div id="app">
     <h2>Calendario</h2>
-    <my-canvas style="width: 3508px; height: 600px;">
+    <!-- 600 -->
+    <my-canvas style="width: 3508px; height: 3548px;">
       <my-cell
         v-for="(obj, index) in schedule"  :key="index"
-        :x="obj.x"
-        :y="obj.y"
         :tipo="obj.tipo"
         :texto="obj.texto"
         :startHour="obj.startHour"
         :endHour="obj.endHour"
+        :transmitionDays="obj.transmitionDays"
       >
       </my-cell>
     </my-canvas>
@@ -29,40 +29,30 @@ export default {
 
   data () {
     return {
-      schedule: [
+      // arrayDaysUno: ['Lunes','Martes'],
+      // arrayDaysDos: ['Lunes','Martes','Miércoles'],
+      schedule: [        
         // {texto: '', tipo: 'hora', x: 0, y: 0, startHour: '', endHour: ''},
         // {texto: '05:15', tipo: 'hora', x: 0, y: 31, startHour: '', endHour: ''},
         // {texto: '05:30', tipo: 'hora', x: 0, y: 71, startHour: '', endHour: ''},
         // {texto: '05:45', tipo: 'hora', x: 0, y: 111, startHour: '', endHour: ''},
         // {texto: '06:00', tipo: 'hora', x: 0, y: 151, startHour: '', endHour: ''},
-        {texto: 'LUNES', tipo: 'cabecera', x: 60, y: 0, startHour: '', endHour: '', transmitionDays:''},
-        {texto: 'MARTES', tipo: 'cabecera', x: 335, y: 0, startHour: '', endHour: '', transmitionDays:''},
-        {texto: 'MIERCOLES', tipo: 'cabecera', x: 610, y: 0, startHour: '', endHour: '', transmitionDays:''},
-        {texto: 'JUEVES', tipo: 'cabecera', x: 885, y: 0, startHour: '', endHour: '', transmitionDays:''},
-        {texto: 'VIERNES', tipo: 'cabecera', x: 1160, y: 0, startHour: '', endHour: '', transmitionDays:''},
-        {texto: 'SABADO', tipo: 'cabecera', x: 1435, y: 0, startHour: '', endHour: '', transmitionDays:''},
-        {texto: 'DOMINGO', tipo: 'cabecera', x: 1710, y: 0, startHour: '', endHour: '', transmitionDays:''},
-        {texto: 'AMERICA NOTICIAS: PRIMERA EDICION: LOCAL (GP)', tipo: 'celda', x: 360, y: (3508 / (24 * 60)) * 15, startHour: 'March 13, 08 05:20', endHour: 'March 13, 08 05:35', transmitionDays:['Lunes','Martes']},
-        {texto: 'AMERICA NOTICIAS: PRIMERA EDICION (GP)', tipo: 'celda', x: 61, y: (3508 / (24 * 60)) * 15 + 25, startHour: 'March 13, 08 05:20', endHour: 'March 13, 08 05:35', transmitionDays:['Martes','Miércoles']},
-        //{texto: 'AMERICA NOTICIAS: PRIMERA EDICION (GP)', tipo: 'celda', x: 100, y: 80, startHour: 'March 13, 08 05:31', endHour: 'March 13, 08 06:30'},
+        // {texto: 'LUNES', tipo: 'cabecera', x: 60, y: 0, startHour: '', endHour: '', transmitionDays:''},
+        // {texto: 'MARTES', tipo: 'cabecera', x: 335, y: 0, startHour: '', endHour: '', transmitionDays:''},
+        // {texto: 'MIERCOLES', tipo: 'cabecera', x: 610, y: 0, startHour: '', endHour: '', transmitionDays:''},
+        // {texto: 'JUEVES', tipo: 'cabecera', x: 885, y: 0, startHour: '', endHour: '', transmitionDays:''},
+        // {texto: 'VIERNES', tipo: 'cabecera', x: 1160, y: 0, startHour: '', endHour: '', transmitionDays:''},
+        // {texto: 'SABADO', tipo: 'cabecera', x: 1435, y: 0, startHour: '', endHour: '', transmitionDays:''},
+        // {texto: 'DOMINGO', tipo: 'cabecera', x: 1710, y: 0, startHour: '', endHour: '', transmitionDays:''},
+        {texto: 'AMERICA NOTICIAS: PRIMERA EDICION: LOCAL (GP)', tipo: 'celda', startHour: 'March 13, 08 05:16', endHour: 'March 13, 08 05:35', transmitionDays: ['Lunes','Martes','Miércoles']},//x: 360, y: (3508 / (24 * 60)) * 15
+        {texto: 'AMERICA NOTICIAS: PRIMERA EDICION (GP)', tipo: 'celda', startHour: 'March 13, 08 05:36', endHour: 'March 13, 08 05:50', transmitionDays: ['Martes','Viernes']},//x: 61, y: (3508 / (24 * 60)) * 15 + 40
+        {texto: 'ESPECTACULOS', tipo: 'celda', startHour: 'March 13, 08 05:51', endHour: 'March 13, 08 06:50', transmitionDays: ['Miércoles','Viernes']}
       ]
-      // schedule: [
-      //   {texto: '', tipo: 'cabecera', x: 0, y: 0, ancho: 100, alto: 40},
-      //   {texto: '05:15', tipo: 'cabecera', x: 0, y: 31, ancho: 100, alto: 40},
-      //   {texto: '05:30', tipo: 'cabecera', x: 0, y: 71, ancho: 100, alto: 40},
-      //   {texto: '05:45', tipo: 'cabecera', x: 0, y: 111, ancho: 100, alto: 40},
-      //   {texto: '06:00', tipo: 'cabecera', x: 0, y: 151, ancho: 100, alto: 40},
-      //   {texto: 'LUNES', tipo: 'cabecera', x: 100, y: 0, ancho: 120, alto: 40},
-      //   {texto: 'MARTES', tipo: 'cabecera', x: 220, y: 0, ancho: 120, alto: 40},
-      //   {texto: 'MIERCOLES', tipo: 'cabecera', x: 340, y: 0, ancho: 120, alto: 40},
-      //   {texto: 'JUEVES', tipo: 'cabecera', x: 460, y: 0, ancho: 120, alto: 40},
-      //   {texto: 'VIERNES', tipo: 'cabecera', x: 580, y: 0, ancho: 120, alto: 40},
-      //   {texto: 'SABADO', tipo: 'cabecera', x: 700, y: 0, ancho: 120, alto: 40},
-      //   {texto: 'DOMINGO', tipo: 'cabecera', x: 820, y: 0, ancho: 120, alto: 40},
-      //   {texto: 'AMERICA NOTICIAS: PRIMERA EDICION: LOCAL (GP)', tipo: 'celda', x: 100, y: 40, ancho: 500, alto: 50},
-      //   {texto: 'AMERICA NOTICIAS: PRIMERA EDICION (GP)', tipo: 'celda', x: 100, y: 80, ancho: 500, alto: 50},
-      // ]
     }
+  },
+  created() {
+    //console.log(this.arrayDaysUno)
+    //console.log(this.arrayDaysDos)
   }
   
 }
--- src/components/MyCell.vue
+++ src/components/MyCell.vue
@@ -6,14 +6,14 @@ const percentHeightToPix = (percent, ctx) => Math.floor((ctx.canvas.height / 100
 export default {
   inject: ['provider'],
   props: {
-    x: {
-      type: Number,
-      default: 0
-    },
-    y: {
-      type: Number,
-      default: 0
-    },
+    // x: {
+    //   type: Number,
+    //   default: 0
+    // },
+    // y: {
+    //   type: Number,
+    //   default: 0
+    // },
     texto: {
       type: String,
       default: ''
@@ -50,8 +50,18 @@ export default {
       ancho: null,
       resolutionWidth: 2480,
       resolutionHigh: 3508,
-      x: null,
-      y: null
+      highHeadboard: null,
+      yPrev: 41,
+      altoPrevCell: null,
+      yPrevLun: null,
+      yPrevMar: null,
+      yPrevMie: null,
+      yPrevJue: null,
+      yPrevVie: null,
+      yPrevSab: null,
+      yPrevDom: null,
+      //x: null,
+      //y: null
       //timeInterval: 15,
       //anchoHora: 60
     }
@@ -74,17 +84,81 @@ export default {
       return calculated
     }
   },
-  // methods: {    
-  //   calcTime(endHour, startHour) {
-  //     this.time = (new Date(this.endHour).getHours() - new Date(this.startHour).getHours())*60
-  //   },
-  //    calcMinAlto () {
-  //     this.alto = 3508 / (24 * 60)
-  //   },
-  //   calcAncho () {
-  //     this.ancho = 2480 / 9
-  //   }
-  // },
+  methods: {
+    createdCell(tipo, transmitionDays) {
+      if(tipo === 'celda') {
+        let hoursEndHour = new Date(this.endHour).getHours()// horas fin
+        let minutesEndHour = new Date(this.endHour).getMinutes()//minutos fin
+        let hoursStartHour = new Date(this.startHour).getHours()// horas empieza
+        let minutesStartHour = new Date(this.startHour).getMinutes()//minutos empieza
+        this.time = ((hoursEndHour * 60) + minutesEndHour) - ((hoursStartHour * 60) + minutesStartHour)
+        this.alto = this.time * (this.resolutionHigh / (24 * 60))
+        this.altoPrevCell = this.alto
+        this.ancho = this.resolutionWidth/7
+        console.log(this.time, 'time')
+        debugger
+        if( hoursStartHour === 5 && minutesStartHour === 16) {
+          console.log(this.highHeadboard, 'highHeadboard')
+          this.y = this.yPrev
+          console.log(this.y, 'y1')
+          // this.yPrev = this.y
+          console.log(this.yPrev, 'yPrev1')
+        } else {
+          this.y = this.yPrev + this.altoPrevCell
+          console.log(this.yPrev + this.altoPrevCell,'suma')
+          console.log(this.y, 'y')        
+          console.log(this.altoPrevCell, 'altoPrevCell')   
+          this.yPrev = this.y
+          console.log(this.yPrev, 'yPrev')
+        }
+        if ( transmitionDays.length !== 0) {
+          switch(transmitionDays[0]) {//transmitionDays[0] indica en que día comienza el horario
+            case "Lunes":
+              //this.yPrevLun = this.y
+              this.x = 0;
+              //this.y = this.highHeadboard + 1
+              //this.y = (3508 / (24 * 60)) * this.time
+              break;
+            case "Martes":
+              //this.yPrevMar = this.y
+              this.x = this.ancho + 1;
+              //.y = (3508 / (24 * 60)) * (this.time * 2)
+              break;
+            case "Miércoles":
+              this.x = (this.ancho) * 2 + 1;
+              break;
+            case "Jueves":
+              this.x = (this.ancho) * 3 + 1;
+              break;
+            case "Viernes":
+              this.x = (this.ancho) * 4 + 1;
+              break;
+            case "Sábado":
+              this.x = (this.ancho) * 5 + 1;
+              break;
+            case "Domingo":
+              this.x = (this.ancho) * 6 + 1;
+            }   
+        }
+      } 
+      else if( tipo === 'cabecera') {
+        this.time = ''
+        this.alto = 40 //36.54
+        this.highHeadboard = this.alto
+        this.ancho = this.resolutionWidth / 7//2480
+        this.y = 0
+      }
+    },    
+    calcTime(endHour, startHour) {
+      this.time = (new Date(this.endHour).getHours() - new Date(this.startHour).getHours())*60
+    },
+     calcMinHightToPixel () {
+      this.alto = this.resolutionHigh / (24 * 60)
+    },
+    calcAncho () {
+      this.ancho = 2480 / 9
+    }
+  },
   render () {
     if(!this.provider.context) return;
     const ctx = this.provider.context;
@@ -150,63 +224,69 @@ export default {
     // ctx.textAlign = 'center';
     // ctx.fillText(this.texto, (this.x + (this.ancho / 2)), (this.y + this.alto - 5));
 
-    console.log(this.alto)
-    console.log(this.ancho)
-    console.log(this.time)
+    console.log(this.alto, 'alto')
+    console.log(this.ancho, 'ancho')
+    console.log(this.time, 'time')
+    console.log(this.tipo, 'tipo')
+    console.log(this.transmitionDays, 'transmitionDays')
   },
   created() {
     //this.alto = 3508 / (24 * 60)
-    //this.ancho = 2480 / 9
-    
-    if(this.tipo === 'celda') {
-      let hoursEndHour = new Date(this.endHour).getHours()// horas fin
-      let minutesEndHour = new Date(this.endHour).getMinutes()//minutos fin
-      let hoursStartHour = new Date(this.startHour).getHours()// horas empieza
-      let minutesStartHour = new Date(this.startHour).getMinutes()//minutos empieza
-      this.time = ((hoursEndHour * 60) + minutesEndHour) - ((hoursStartHour * 60) + minutesStartHour)
-      this.alto = this.time * (this.resolutionHigh / (24 * 60))
-      this.ancho = this.resolutionWidth/7
-      console.log(this.time, 'fff')
-      if (this.transmitionDays.length !== 0) {
-        for(i = 0; i < this.transmitionDays.length; i++){
-          switch(this.transmitionDays) {
-          case "Lunes":
-            this.x = 60;
-            break;
-          case "Martes":
-            this.x = 335;
-            break;
-          case "Miércoles":
-            this.x = 610;
-            break;
-          case "Jueves":
-            this.x = 885;
-            break;
-          case "Viernes":
-            this.x = 1160;
-            break;
-          case "Sábado":
-            this.x = 1435;
-            break;
-          case "Domingo":
-            this.x = 1710;
-          }
-      }
+    //this.ancho = 2480 / 9    
+    this.createdCell(this.tipo, this.transmitionDays)
+    console.log(this.transmitionDays, 'transmitionDays created')
+    // if(this.tipo === 'celda') {
+    //   let hoursEndHour = new Date(this.endHour).getHours()// horas fin
+    //   let minutesEndHour = new Date(this.endHour).getMinutes()//minutos fin
+    //   let hoursStartHour = new Date(this.startHour).getHours()// horas empieza
+    //   let minutesStartHour = new Date(this.startHour).getMinutes()//minutos empieza
+    //   this.time = ((hoursEndHour * 60) + minutesEndHour) - ((hoursStartHour * 60) + minutesStartHour)
+    //   this.alto = this.time * (this.resolutionHigh / (24 * 60))
+    //   this.ancho = this.resolutionWidth/7
+    //   console.log(this.time, 'fff')
+    //   if (this.transmitionDays.length !== 0) {
+    //     for(i = 0; i < this.transmitionDays.length; i++){
+    //       switch(this.transmitionDays) {
+    //       case "Lunes":
+    //         this.x = 60;
+    //         this.y = (3508 / (24 * 60)) * this.time
+    //         break;
+    //       case "Martes":
+    //         this.x = 335;
+    //         this.y = (3508 / (24 * 60)) * this.time
+    //         break;
+    //       case "Miércoles":
+    //         this.x = 610;
+    //         this.y = (3508 / (24 * 60)) * this.time
+    //         break;
+    //       case "Jueves":
+    //         this.x = 885;
+    //         break;
+    //       case "Viernes":
+    //         this.x = 1160;
+    //         break;
+    //       case "Sábado":
+    //         this.x = 1435;
+    //         break;
+    //       case "Domingo":
+    //         this.x = 1710;
+    //       }
+    //   }
       
       
-      }
-    }    
+    //   }
+    // }    
     // else if(this.tipo === 'hora') {
     //   this.time = ''
     //   this.alto = (this.resolutionHigh / (24 * 60)) * this.timeInterval//36.5416..
     //   this.ancho = 60
     //   this.anchoHora = this.ancho
     // }
-    else if(this.tipo === 'cabecera') {
-      this.time = ''
-      this.alto = (this.resolutionHigh / (24 * 60)) * 15
-      this.ancho = this.resolutionWidth / 7//2480
-    }
+    // else if(this.tipo === 'cabecera') {
+    //   this.time = ''
+    //   this.alto = (this.resolutionHigh / (24 * 60)) * 15
+    //   this.ancho = this.resolutionWidth / 7//2480
+    // }
     
   }
 }
